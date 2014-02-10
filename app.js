dc91181f86584092951cb3e323a70054c9573a29
Rudi Sherry <rudisherry666@gmail.com>
2014-02-09 16:11:53 -0800

    changed port to 8088

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function(){
-    app.set('port', process.env.PORT || 80);
+    app.set('port', process.env.PORT || 8088);
     app.set('views', __dirname + '/views');
     // app.engine('html', require('ejs').renderFile);
     app.use(express.favicon());
