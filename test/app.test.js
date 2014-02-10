b2b75e7a50231160093864e07fcf621546f8aeb9
Rudi Sherry <rudisherry666@gmail.com>
2014-02-09 17:03:49 -0800

    Updated test to reflect new port number.

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,7 @@ var app = require('../app'),
 describe('GET /', function(){
 
   it('should return a 200 status code', function (done){
-    http.get({ host: '0.0.0.0', port: 3000 }, function(res) {
+    http.get({ host: '0.0.0.0', port: 8088 }, function(res) {
       assert.deepEqual(res.statusCode, 200)
       done();
     }).on('error', function(e) {
