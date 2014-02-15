9b27c2988672eab534f22a6906fa8d3a9b40cb2e
Rudi Sherry <rsherry@adobe.com>
2014-02-15 13:50:44 -0800

    All tiles done with CSS.
    Still need to remove tables and code that implements some of it in javascript.

--- models/pgdot.js
+++ models/pgdot.js
@@ -13,23 +13,25 @@
 */
 var locations = [
   [  0,  0 ],   //  0 placeholder: dots start at one
+                //  -------------- TOP HALF ---------------
   [  4,  6 ],   //  1 top left
   [  4, 17 ],   //  2 top middle
   [  4, 28 ],   //  3 top right
-  [ 24,  6 ],   //  4 upper-middle left
-  [ 24, 17 ],   //  5 upper-middle middle
-  [ 24, 28 ],   //  6 upper-middle right
-  [ 34,  6 ],   //  7 
-  [ 34, 28 ],   //  8
-  [ 44,  6 ],   //  9
-  [ 44, 28 ],   // 10 middle left
-  [ 64,  6 ],   // 11 middle right
-  [ 64, 28 ],   // 12
-  [ 74,  6 ],   // 13 
-  [ 74, 28 ],   // 14
-  [ 84,  6 ],   // 15 lower-middle left
-  [ 84, 17 ],   // 16 lower-middle middle
-  [ 84, 28 ],   // 17 lower-middle right
+  [ 24,  6 ],   //  4 middle left
+  [ 24, 17 ],   //  5 middle middle
+  [ 24, 28 ],   //  6 middle right
+  [ 34,  6 ],   //  7 lower-middle left
+  [ 34, 28 ],   //  8 lower-middle right
+  [ 44,  6 ],   //  9 bottom left
+  [ 44, 28 ],   // 10 bottom right
+                //  -------------- BOTTOM HALF ---------------
+  [ 64,  6 ],   // 11 top left
+  [ 64, 28 ],   // 12 top right
+  [ 74,  6 ],   // 13 upper-middle left
+  [ 74, 28 ],   // 14 upper-middle right
+  [ 84,  6 ],   // 15 middle left
+  [ 84, 17 ],   // 16 middle middle
+  [ 84, 28 ],   // 17 middle right
   [104,  6 ],   // 18 bottom left
   [104, 17 ],   // 19 bottom middle
   [104, 28 ],   // 20 bottom right
@@ -46,7 +48,7 @@ function PGDot(index, color) {
     if (color !== 'red' && color !== 'white') throw "PGDot: bad color " + color;
     if (index < 1 && index >= locations.length) throw "PGDot: bad index " + index;
     this._index = index;
-    this._color = color;    
+    this._color = color;
 }
 
 /*
@@ -60,7 +62,7 @@ PGDot.prototype.json = function() {
         color: this._color,
         location: this.location()
     };
-}
+};
 
 
 /*
@@ -75,7 +77,7 @@ PGDot.prototype.json = function() {
 */
 PGDot.prototype.location = function() {
     return locations[this._index];
-}
+};
 
 
 module.exports = PGDot;
