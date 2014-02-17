04247743cd9cd55e94646eff858bb88ac6875ecc
Rudi Sherry <rsherry@adobe.com>
2014-02-17 07:54:54 -0800

    require, jquery, backbone, underscore added.

---
+++ static/js/paigow.require-main.js
@@ -0,0 +1,31 @@
+/*
+* The main require configuration for paigow.
+*
+* Taken from <http://requirejs.org/docs/api.html#config>
+*
+*/
+requirejs.config({
+    baseUrl: "js",
+    paths: {
+        'backbone': "lib/backbone-1.1.1.min",
+        'bootstrap': "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min",
+        'jquery': "//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min",
+        'underscore': "lib/underscore-1.6.0.min"
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'underscore': {
+            exports: '_'
+        }
+    }
+});
+
+define(['bootstrap', 'backbone'], function(bs, bb) {
+
+});
