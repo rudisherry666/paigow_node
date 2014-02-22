8de3f8a0de45aad848ad4de825367fa1c831933d
Rudi Sherry <rsherry@adobe.com>
2014-02-22 13:11:39 -0800

    de-lint.

--- test/app.test.js
+++ test/app.test.js
@@ -7,7 +7,7 @@ describe('GET /', function(){
 
   it('should return a 200 status code', function (done){
     http.get({ host: '0.0.0.0', port: 8088 }, function(res) {
-      assert.deepEqual(res.statusCode, 200)
+      assert.deepEqual(res.statusCode, 200);
       done();
     }).on('error', function(e) {
       throw new Error(e);
