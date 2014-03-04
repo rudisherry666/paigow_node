fd58257701da136f31527158cd58c99d4920febf
Rudi Sherry <rsherry@adobe.com>
2014-03-04 11:10:54 -0800

    more deferseq tests.

--- test/utils/deferseq.test.js
+++ test/utils/deferseq.test.js
@@ -5,7 +5,7 @@ console.log("test: DeferSeq");
 
 describe('DeferSeq', function() {
 
-    it('should add a function and execute it', function(done) {
+    it('should add a function and execute it with the correct param', function(done) {
         var paramIn = 'hello';
         DeferSeq.add('test1', function doMe(defer, err, param) {
             assert.equal(err, null);
@@ -17,16 +17,29 @@ describe('DeferSeq', function() {
 
     it('should execute in order', function(done) {
         var firstDone = false;
-        function doMeFirst(defer, err, param) {
+        function doMeFirst2(defer, err, param) {
             firstDone = true;
             defer.resolve();
         }
-        function doMeSecond(defer, err, param) {
+        function doMeSecond2(defer, err, param) {
             assert(firstDone, "second done before first finished!");
             defer.resolve();
             done();
         }
-        DeferSeq.add('test2', doMeFirst, null);
-        DeferSeq.add('test2', doMeSecond, null);
+        DeferSeq.add('test2', doMeFirst2, null);
+        DeferSeq.add('test2', doMeSecond2, null);
+    });
+
+    it('should pass error correctly from previous function', function(done) {
+        function doMeFirst3(defer, err, param) {
+            defer.reject("doMeFirst3");
+        }
+        function doMeSecond3(defer, err, param) {
+            assert.equal(err, "doMeFirst3");
+            defer.resolve();
+            done();
+        }
+        DeferSeq.add('test2', doMeFirst3, null);
+        DeferSeq.add('test2', doMeSecond3, null);
     });
 });
