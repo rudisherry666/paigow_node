f48b8e789ba5d9b13d28692297f456f2808967fb
Rudi Sherry <rsherry@adobe.com>
2014-03-04 11:04:44 -0800

    More deferseq tests.

--- test/utils/deferseq.test.js
+++ test/utils/deferseq.test.js
@@ -6,8 +6,27 @@ console.log("test: DeferSeq");
 describe('DeferSeq', function() {
 
     it('should add a function and execute it', function(done) {
-        // This will time out if it's not done.
-        function doMe() { done(); }
-        DeferSeq.add('test', doMe);
+        var paramIn = 'hello';
+        DeferSeq.add('test1', function doMe(defer, err, param) {
+            assert.equal(err, null);
+            assert.equal(param, paramIn);
+            defer.resolve();
+            done();
+        }, paramIn);
+    });
+
+    it('should execute in order', function(done) {
+        var firstDone = false;
+        function doMeFirst(defer, err, param) {
+            firstDone = true;
+            defer.resolve();
+        }
+        function doMeSecond(defer, err, param) {
+            assert(firstDone, "second done before first finished!");
+            defer.resolve();
+            done();
+        }
+        DeferSeq.add('test2', doMeFirst, null);
+        DeferSeq.add('test2', doMeSecond, null);
     });
 });
