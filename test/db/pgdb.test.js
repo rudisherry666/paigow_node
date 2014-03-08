fb38a6ec8a174efe81d950bed8d2e59e044edf4b
Rudi Sherry <rsherry@adobe.com>
2014-03-08 10:50:01 -0800

    More pgdb tests

--- test/db/pgdb.test.js
+++ test/db/pgdb.test.js
@@ -21,12 +21,14 @@ describe('PGDB', function() {
         assert.equal(pgdb1.fullTableName(), 'test-test1');
         assert.equal(pgdb2.fullTableName(), 'test-test2');
     });
+
     it('should allow adding an item', function(done) {
         pgdb1.add({key1: 'foo'}).then(
             function(data) { },
-            function(err)  { assert.fail('cannot add an item: ' + err); }
+            function(err)  { assert.fail("cannot add an item: " + err); }
         ).done(function()  { done(); });
     });
+
     it('should find an added item', function(done) {
         var item = {key1: 'foo2'};
         pgdb1.add(item).then(
@@ -39,26 +41,52 @@ describe('PGDB', function() {
                         assert.equal(typeof data[0], "object");
                         assert.deepEqual(data[0], item);
                     },
-                    function(err)  { assert.fail('cannot find an item: ' + err); }
+                    function(err)  { assert.fail("cannot find an item: " + err); }
                 ).done(function()  { done(); });
             },
             function(err)  {
-                assert.fail('cannot add an item: ' + err);
+                assert.fail("cannot add an item: " + err);
                 done();
             }
         );
     });
+
     it('should allow deleting an added item', function(done) {
         var deleteItemVal = "fooDelete";
         pgdb1.add({key1: deleteItemVal}).then(
             function(data) {
                 pgdb1.delete(deleteItemVal).then(
                     function(data) { },
-                    function(err)  { assert.fail('cannot delete an item: ' + err); }
+                    function(err)  { assert.fail("cannot delete an item: " + err); }
                 ).done(function()  { done(); });
             },
             function(err)  {
-                assert.fail('cannot add an item for deletion: ' + err);
+                assert.fail("cannot add an item for deletion: " + err);
+                done();
+            }
+        );
+    });
+
+    it('should allow adding properties', function(done) {
+        var item = { key1: "add-prop" };
+        var finalItem = {
+            key1: "add-prop",
+            foo2: "added"
+        };
+        pgdb1.add(item).then(
+            function(data) {
+                pgdb1.set("foo2", "added").then(
+                    function(data) {
+                        pgdb1.find('add-prop').then(
+                            function(data) { assert.deepEqual(data[0], finalItem); },
+                            function(err)  { assert.fail("cannot find add-prop item " + err); }
+                        ).done(function()  { done(); });
+                    },
+                    function(err)  { assert.fail("cannot set-prop an item: " + err); }
+                );
+            },
+            function(err)  {
+                assert.fail("cannot add an item for prop-adding: " + err);
                 done();
             }
         );
